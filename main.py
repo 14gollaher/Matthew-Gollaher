97cde0541de0279f0454fa2eba7b4570dfdb4700
Unknown <mattgollaher@gmail.com>
2017-10-27 18:38:49 -0500

    Added additional files for first release

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/index')
+@app.route('/home')
+@app.route('/')
 def index():
     return render_template('index.html')
