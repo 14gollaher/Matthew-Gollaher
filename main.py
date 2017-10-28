44f42aae8ed28917fbb1eadc2bfe244cdab9893e
Unknown <mattgollaher@gmail.com>
2017-10-27 18:22:04 -0700

    Added Cache-Money-Heroes game to web app.

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/cache-money-heroes')
+def index():
+    return render_template('cache-money-heroes.html')
